75e8d899a329457e6fb86983c4efb9c371fe165b
VadimTalimonchik <tvadmins@gmail.com>
2022-12-12 18:26:07 +0300

    Добавил папку hw_27

---
+++ hw_27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27.
+// Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+// int SumNum(int num)
+// {
+//     int sum = 0;
+
+    while (number != 0)
+    {
+        number = number / 10;
+    }
+    int num = number % 10;
+    Console.WriteLine(num);
+
+// }
